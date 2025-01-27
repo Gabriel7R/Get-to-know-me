1bf4b99bfcd3f05975016b4200b6918c99513e67
Gabriel Rodrigues <161777911+Gabriel7R@users.noreply.github.com>
2025-01-27 16:24:23 -0300

    Delete curso JS 2.0 directory

--- curso JS 2.0/Calculadora/calculadora.js
+++
@@ -1,6 +0,0 @@
-let numero = prompt("Coloque o numero que voce deseja fazer a tabuada:")
-let resultado = ""
-
-for( let fator = 1; fator <= 10; fator++){
-  resultado += "->" + numero + " * " + fator + " = " + (numero * fator) + "\n\n" 
-} alert("Resultado da tabuada de " + numero + ":\n\n" + resultado)
--- curso JS 2.0/Curso JS yt/Aula 06/ex0062.js
+++
@@ -1,7 +0,0 @@
-var pais= 'EUA'
-console.log(`Vivendo em ${pais}`)
-if (pais=='Brasil') {
-    console.log('Voce é brasileiro')
-} else {
-    console.log('Voce é estrangeiro')
-}
